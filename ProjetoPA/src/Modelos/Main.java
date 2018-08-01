c55a5a888236c25ae346a2e167fab6e14546a59f
nmpetry <nmpetry@LAB1-25-WIN.ROCHAS>
2018-07-31 22:04:12 -0300

    criada classe Main para inicialização

---
+++ ProjetoPA/src/Modelos/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+/**
+ *
+ * @author nmpetry
+ */
+public class Main {
+     public static void main(String args[]) {
+         //INICIALIZAÇÃO
+         Principal tela = new Principal();
+         tela.setVisible(true);
+     }
+}
